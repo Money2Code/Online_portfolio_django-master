64413c63428524d3afcf55a0708d687ce0f9c992
Abhinav <135271392+Money2Code@users.noreply.github.com>
2024-05-13 12:32:04 -0400

    setting Change

--- portal/views.py
+++ portal/views.py
@@ -12,35 +12,8 @@ def home(request):
         mobile_number=request.POST['mobile_number']
         email_subject=request.POST['email_subject']
         descsription=request.POST['descsription']
-
         new_contact=contact_details(Full_name=Full_name, email=email, mobile_number=mobile_number, email_subject=email_subject, descsription=descsription)
-        new_contact.save()
-
-        # send_mail(
-        #     'Contact',
-        #     descsription,
-        #     settings.EMAIL_HOST_USER,
-        #     [email,],
-        #     fail_silently=False
-        # )
-        
-        # with get_connection(  
-        #    host=settings.EMAIL_HOST, 
-        #     port=settings.EMAIL_PORT,  
-        #     username=settings.EMAIL_HOST_USER, 
-        #     password=settings.EMAIL_HOST_PASSWORD, 
-        #     use_tls=settings.EMAIL_USE_TLS  
-        #  ) as connection:  
-        #    subject = request.POST.get("email_subject")  
-        #    email_from = settings.EMAIL_HOST_USER  
-        #    recipient_list = [request.POST.get("email"), ]  
-        #    message = request.POST.get("descsription")  
-        #    EmailMessage(subject, message, email_from, recipient_list, connection=connection).send()  
- 
-        # try:
-        #     send_mail(email_subject,descsription,email,['abhinavdogra613@gmail.com'])
-        # except BadHeaderError:
-        #     return HttpResponse("Invalid header found") 
+        new_contact.save() 
         email = EmailMessage('Subject', 'Body', to=[email])
         email.send()
     return render(request,'index.html')
